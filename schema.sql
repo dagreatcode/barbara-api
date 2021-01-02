4e824be81ad2659fe9b8654edcd5da701a17a420
dagreatcode <dagreatcode@gmail.com>
2021-01-02 09:14:22 -0500

    schema done for now

--- schema.sql
+++ schema.sql
@@ -21,11 +21,6 @@ CREATE TABLE retroluxe (
 -- Insert a set of records.
 INSERT INTO poohmadeit (name, bin_location, description) VALUES ("First Jacket", 1, "relaxed");
 INSERT INTO poohmadeit (name, bin_location, description) VALUES ("First Pants", 1, "righteously nice");
-INSERT INTO poohmadeit (name, bin_location, description) VALUES ("Kramer", 20, "doofus");
-INSERT INTO poohmadeit (name, bin_location, description) VALUES ("George", 70, "selfish");
 
 -- Insert a set of records.
-INSERT INTO retroluxe (name, bin_location, description) VALUES ("Jerry", 90, "relaxed");
-INSERT INTO retroluxe (name, bin_location, description) VALUES ("Elaine", 80, "righteous");
-INSERT INTO retroluxe (name, bin_location, description) VALUES ("Kramer", 20, "doofus");
-INSERT INTO retroluxe (name, bin_location, description) VALUES ("George", 70, "selfish");
+INSERT INTO retroluxe (name, bin_location, description) VALUES ("Project Coming Soon", 0, "relaxed");
