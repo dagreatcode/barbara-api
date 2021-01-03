83408532f75511d89f0583ab545d178c8d4d5b67
dagreatcode <dagreatcode@gmail.com>
2021-01-03 09:24:28 -0500

    n/a

--- server.js
+++ server.js
@@ -92,111 +92,112 @@ app.get("/api/config", (res, req) => {
 // });
 
 
-// // Views Routes
-// app.get("/", function(req, res) {
-//     // res.json(path.join(__dirname, "public/index.html"));
-//     res.send(`<h1>Best Database</h1>`);
-// });
+// Views Routes
+app.get("/", function(req, res) {
+    // res.json(path.join(__dirname, "public/index.html"));
+    res.send(`<h1>Best Database</h1>`);
+});
 
-// app.get("/new", function(req, res) {
-//     // res.json(path.join(__dirname, "public/index.html"));
-//     res.send(`<h1>Best Database</h1>`);
-// });
+app.get("/new", function(req, res) {
+    // res.json(path.join(__dirname, "public/index.html"));
+    res.send(`<h1>Best Database</h1>`);
+});
 
-// app.get("edit/:id", function(req, res) {
-//     // res.json(path.join(__dirname, "public/index.html"));
-//     res.send(`<h1>Best Database</h1>`);
-// });
+app.get("edit/:id", function(req, res) {
+    // res.json(path.join(__dirname, "public/index.html"));
+    res.send(`<h1>Best Database</h1>`);
+});
 
-// app.get("/index", function(req, res) {
-//     connection.query("SELECT * FROM poohmadeit WHERE name", function(err, result) {
-//         res.render("index", );
-//     });
-// });
+app.get("/index", function(req, res) {
+    connection.query("SELECT * FROM poohmadeit WHERE name", function(err, result) {
+        res.render("index", );
+    });
+});
 
-// app.get("/poohmadeit", function(req, res) {
-//   connection.query("SELECT * FROM poohmadeit ORDER BY id", function(err, result) {
-//     if (err) throw err;
+app.get("/poohmadeit", function(req, res) {
+  connection.query("SELECT * FROM poohmadeit ORDER BY id", function(err, result) {
+    if (err) throw err;
     
-//     const html = "<h1>Pooh Made It</h1>";
+    const html = "<h1>Pooh Made It</h1>";
 
-//     html += "<ul>";
+    html += "<ul>";
 
-//     for (let i = 0; i < result.length; i++) {
-//       html += "<li><p> ID: " + result[i].id + "</p>";
-//       html += "<p> Name: " + result[i].name + "</p>";
-//       html += "<p> bin_location: " + result[i].bin_location + "</p>";
-//       html += "<p>description: " + result[i].description + "</p></li>";
-//     }
+    for (let i = 0; i < result.length; i++) {
+      html += "<li><p> ID: " + result[i].id + "</p>";
+      html += "<p> Name: " + result[i].name + "</p>";
+      html += "<p> bin_location: " + result[i].bin_location + "</p>";
+      html += "<p>description: " + result[i].description + "</p></li>";
+    }
 
-//     html += "</ul>";
+    html += "</ul>";
 
-//     res.send(html);
-//   });
-// });
+    res.send(html);
+  });
+});
 
-// app.get("/poohmadeit/:bin", function(req, res) {
-//     connection.query("SELECT * FROM poohmadeit WHERE bin_location = ?", [req.params.bin], function(err, result) {
-//         if (err) throw err;
+app.get("/poohmadeit/:bin", function(req, res) {
+    connection.query("SELECT * FROM poohmadeit WHERE bin_location = ?", [req.params.bin], function(err, result) {
+        if (err) throw err;
     
-//         const html = "<h1>Bin's " + req.params.bin + "</h1>";
+        const html = "<h1>Bin's " + req.params.bin + "</h1>";
     
-//         html += "<ul>";
+        html += "<ul>";
     
-//         for (let i = 0; i < result.length; i++) {
-//           html += "<li><p> ID: " + result[i].id + "</p>";
-//           html += "<p> Name: " + result[i].name + "</p>";
-//           html += "<p> bin_location: " + result[i].bin_location + "</p>";
-//           html += "<p>description: " + result[i].description + "</p></li>";
-//         }
+        for (let i = 0; i < result.length; i++) {
+          html += "<li><p> ID: " + result[i].id + "</p>";
+          html += "<p> Name: " + result[i].name + "</p>";
+          html += "<p> bin_location: " + result[i].bin_location + "</p>";
+          html += "<p>description: " + result[i].description + "</p></li>";
+        }
     
-//         html += "</ul>";
+        html += "</ul>";
     
-//         res.send(html);
-//       });
-// });
+        res.send(html);
+      });
+});
 
-// app.get("/retroluxe", function(req, res) {
-//   connection.query("SELECT * FROM retroluxe ORDER BY bin_location DESC", function(err, result) {
-//     if (err) throw err;
+app.get("/retroluxe", function(req, res) {
+  connection.query("SELECT * FROM retroluxe ORDER BY bin_location DESC", function(err, result) {
+    if (err) throw err;
 
-//     const html = "<h1>Retro Luxe</h1>";
+    const html = "<h1>Retro Luxe</h1>";
 
-//     html += "<ul>";
+    html += "<ul>";
 
-//     for (let i = 0; i < result.length; i++) {
-//       html += "<li><p> ID: " + result[i].id + "</p>";
-//       html += "<p> Name: " + result[i].name + "</p>";
-//       html += "<p> bin_location: " + result[i].bin_location + "</p>";
-//       html += "<p>description: " + result[i].description + "</p></li>";
-//     }
+    for (let i = 0; i < result.length; i++) {
+      html += "<li><p> ID: " + result[i].id + "</p>";
+      html += "<p> Name: " + result[i].name + "</p>";
+      html += "<p> bin_location: " + result[i].bin_location + "</p>";
+      html += "<p>description: " + result[i].description + "</p></li>";
+    }
 
-//     html += "</ul>";
+    html += "</ul>";
 
-//     res.send(html);
-//   });
-// });
+    res.send(html);
+  });
+});
 
-// app.get("/retroluxe/:bin", function(req, res) {
-//     connection.query("SELECT * FROM retroluxe WHERE bin_location = ?", [req.params.bin], function(err, result) {
-//         if (err) throw err;
+app.get("/retroluxe/:bin", function(req, res) {
+    connection.query("SELECT * FROM retroluxe WHERE bin_location = ?", [req.params.bin], function(err, result) {
+        if (err) throw err;
     
-//         const html = "<h1>Bin's " + req.params.bin + "</h1>";
+        const html = "<h1>Bin's " + req.params.bin + "</h1>";
     
-//         html += "<ul>";
+        html += "<ul>";
     
-//         for (let i = 0; i < result.length; i++) {
-//           html += "<li><p> ID: " + result[i].id + "</p>";
-//           html += "<p> Name: " + result[i].name + "</p>";
-//           html += "<p> bin_location: " + result[i].bin_location + "</p>";
-//           html += "<p>description: " + result[i].description + "</p></li>";
-//         }
+        for (let i = 0; i < result.length; i++) {
+          html += "<li><p> ID: " + result[i].id + "</p>";
+          html += "<p> Name: " + result[i].name + "</p>";
+          html += "<p> bin_location: " + result[i].bin_location + "</p>";
+          html += "<p>description: " + result[i].description + "</p></li>";
+        }
     
-//         html += "</ul>";
+        html += "</ul>";
     
-//         res.send(html);
-//       });
-// });
+        res.send(html);
+      });
+});
+
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
