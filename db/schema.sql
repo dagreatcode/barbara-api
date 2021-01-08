233d3501b53b9bdd898e0fc17672804ab6d37b9e
dagreatcode <dagreatcode@gmail.com>
2021-01-08 17:17:42 -0500

    great project

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,13 @@
 DROP DATABASE retroluxe_db;
 CREATE DATABASE retroluxe_db;
 USE retroluxe_db;
+USE `rl3zojbpssol3ahj`;
 -- Create the table actors.
 -- Create the table actors.
 CREATE TABLE `poohmadeit` (
   `id` INT AUTO_INCREMENT,
   `name` VARCHAR (30) NOT NULL,
-  `bin_location` INT  NOT NULL,
+  `bin_location` INT NOT NULL,
   `description` VARCHAR (60) NOT NULL,
   `img` LONGBLOB,
   `created_at` DATETIME NOT NULL,
@@ -21,6 +22,19 @@ CREATE TABLE retroluxe (
   timestamp int,
   PRIMARY KEY(id)
 );
-
-select * from retroluxe;
-select * from poohmadeit;
+CREATE TABLE poohmadeit (
+  id int AUTO_INCREMENT,
+  name varchar(30) NOT NULL,
+  bin_location int NOT NULL,
+  description varchar(60) NOT NULL,
+  img LONGBLOB,
+  PRIMARY KEY(id)
+);
+select
+  *
+from
+  retroluxe;
+select
+  *
+from
+  poohmadeit;
